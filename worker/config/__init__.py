044e3f5a932ef13e42f9c258dc3e5454608bc2aa
wasosky313 <wasosky313@gmail.com>
2021-04-30 15:38:58 -0300

    Updated Finish

--- worker/config/__init__.py
+++ worker/config/__init__.py
@@ -1,16 +1,15 @@
 import os
 
-TITLE = "User Image System"
-DESCRIPTION = "This system shall store users images along the time," \
-              " and as a banking application, it may be audited."
+TITLE = "Worker for User Image System logs"
+DESCRIPTION = "Service to store the logs of user-image-api,"
 VERSION = "1.0.0"
 
 # DATABASE
 DB_PORT = os.environ.get("DB_PORT", 5432)
 DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_NAME = os.environ.get("DB_NAME", "user_image_db")
-DB_USER = os.environ.get("DB_USER", "wasa")
-DB_PASS = os.environ.get("DB_PASS", "wasaloko")
+DB_USER = os.environ.get("DB_USER", "user_image")
+DB_PASS = os.environ.get("DB_PASS", "user_image_2021")
 DB_SCHEMA = "user_system_image"
 
 # RabbitMQ
